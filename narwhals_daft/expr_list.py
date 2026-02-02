1a74c7e718a2bd79b3a47559610307d7e892a30c
raisadz <34237447+raisadz@users.noreply.github.com>
2026-02-02 11:52:15 +0000

    feat: add `list.get` and `list.median`

--- narwhals_daft/expr_list.py
+++ narwhals_daft/expr_list.py
@@ -45,15 +45,35 @@ class ExprListNamespace(ListNamespace["DaftExpr"]):
         return self.compliant._with_elementwise(
             lambda expr: F.list_sort(expr, desc=descending, nulls_first=not nulls_last)
         )
-    
+
     def unique(self) -> DaftExpr:
         def func(expr: Expression) -> Expression:
             expr_distinct = F.list_distinct(expr)
-            return F.when(F.list_count(expr, "null") == lit(0), expr_distinct).otherwise(
-                F.list_append(expr_distinct, lit(None))
+            return F.when(
+                F.list_count(expr, "null") == lit(0), expr_distinct
+            ).otherwise(F.list_append(expr_distinct, lit(None)))
+
+        return self.compliant._with_elementwise(func)
+
+    def get(self, index: int) -> DaftExpr:
+        return self.compliant._with_elementwise(lambda expr: F.get(expr, key=index))
+
+    def median(self) -> DaftExpr:
+        def func(expr: Expression) -> Expression:
+            sorted_expr = F.list_sort(expr, nulls_first=False)
+            size = F.list_count(sorted_expr, mode="valid")
+            mid_index = (size / lit(2)).cast("int").fill_null(0)
+            odd_case = F.get(sorted_expr, key=mid_index)
+            even_case = (
+                F.get(sorted_expr, key=mid_index - lit(1))
+                + F.get(sorted_expr, key=mid_index)
+            ) / lit(2)
+            return (
+                F.when((size.is_null()) | (size == lit(0)), lit(None))
+                .when(size % lit(2) == lit(1), odd_case)
+                .otherwise(even_case)
             )
+
         return self.compliant._with_elementwise(func)
 
     contains = not_implemented()
-    get = not_implemented()
-    median = not_implemented()
